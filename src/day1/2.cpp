a826bc21e94595b7cc42d10a7c773d6d216e6b86
valenrc <vvalenrc@gmail.com>
2025-03-05 20:41:45 -0300

    day 1

---
+++ src/day1/2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+using namespace std;
+
+int main(){
+	int x;
+	
+	cin >> x;
+	while(x!= -1){
+		int res = 0;
+		vector<int> list;
+		
+		while(x!=0){
+			list.push_back(x);
+			cin >> x;
+		}
+		
+		for(int i=0; i<list.size(); i++){
+			for(int j=i+1; j<list.size(); j++){
+				if(list[i]*2 == list[j] || list[j]*2 == list[i]){
+					res++;
+				}
+			}
+		}
+		cout << res << "\n";
+		cin >> x;		
+	}
+	return 0;
+}
---
+++ src/day1/3.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+using namespace std;
+
+bool is_prime(int x, vector<int> primos){
+  for(int i=0; i<primos.size(); i++){
+    if(x%primos[i]==0) return false;
+  }
+  return true;
+}
+
+vector<int> generar_primos(){
+  // genera todos los primos menores a 10000
+  vector<int> primos;
+
+  for(int i=2; i<10000; i++){
+    if(is_prime(i, primos)){
+      primos.push_back(i);
+    }
+  }
+
+  return primos;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+
+  // generar array con los numeros primos menores a 10000
+  vector<int> primos = generar_primos();
+	
+	int x;
+	cin >> x;
+	while(x){
+		int res = 0;
+
+    // sliding window ftw
+    int i=0, sum=0;
+      for(int j=0; j<primos.size(); j++){
+      sum += primos[j];
+      
+      while(sum > x){
+        sum -= primos[i];
+        i++;
+      }
+
+      if(sum==x) res++;
+
+      if(primos[i] > x){
+        break;
+      }
+    }
+		
+		cout << res << endl;
+		cin >> x;
+	}
+	
+	return 0;
+}
